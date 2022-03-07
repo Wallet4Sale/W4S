0cfa6a2613b6c78086c205847f6ab30f599892fc
Wallet4Sales <facebook@optimasystems.es>
2022-03-07 15:09:25 -0500

    composer

--- src/Wallet4SalesPHP/Wallet4Sales.php
+++ src/Wallet4SalesPHP/Wallet4Sales.php
@@ -11,7 +11,7 @@ require __DIR__ . '/../../vendor/autoload.php';
 class Wallet4Sales
 {
 
-	private $EndPoint = 'https://www.w4s.ai/apiwallet4sales';
+	private $EndPoint = 'https://www.optimacrm.ai/wallet4sales/apiwallet4sales/';
 
 	private $access_token;
 
@@ -25,59 +25,66 @@ class Wallet4Sales
 	}
 
 	private function doQueryDev($path, $data = null, $Method = 'GET') {
-			// Prepare URL
-			$path = str_replace(" ", "%20", $path);
-			$url = $this->EndPoint. '/' . $path;
-			$headers = array(
-					"Content-Type: application/json",
-					"Authorization: Bearer $this->access_token"
-			);
+		// Prepare URL
+		$path = str_replace(" ", "%20", $path);
+		$url = $this->EndPoint. '/' . $path;
+		$headers = array(
+			"Content-Type: application/json",
+			"Authorization: Bearer $this->access_token"
+		);
 
-			// initiate curl
-			$ch = curl_init($url);
-		
-			$dataString = '';
-			if($data != null) {
-				$Method = "POST";
-				uksort($data, 'strcmp');
-				$post = array();
-				
-				// Loop through every data entry - only do this for non image method
-				foreach($data as $key => $value) {
-					// if (strpos($value, '@') === 0 ) {
-					// 	$value = "\0".$value;
-					// }
-					$post[$key] = $value;
-					// $dataString .= rawurlencode($key) . '=' . rawurlencode($value) . "&";
-				}
-				
-				// $dataString = substr($dataString, 0, -1);
-				// Set cURL post options
-				curl_setopt($ch,CURLOPT_CUSTOMREQUEST, $Method);
-				curl_setopt ($ch, CURLOPT_POSTFIELDS, json_encode($post));
+		// initiate curl
+		$ch = curl_init($url);
+	
+		$dataString = '';
+		if($data != null) {
+			$Method = "POST";
+			uksort($data, 'strcmp');
+			$post = array();
+			
+			// Loop through every data entry - only do this for non image method
+			foreach($data as $key => $value) {
+				// if (strpos($value, '@') === 0 ) {
+				// 	$value = "\0".$value;
+				// }
+				$post[$key] = $value;
+				// $dataString .= rawurlencode($key) . '=' . rawurlencode($value) . "&";
 			}
 			
-			curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch,CURLOPT_ENCODING, '');
-			curl_setopt($ch,CURLOPT_MAXREDIRS, 10);
-			curl_setopt($ch,CURLOPT_TIMEOUT, 0);
-			curl_setopt($ch,CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch,CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-			curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
+			// var_dump(json_encode($post));
+			// $dataString = substr($dataString, 0, -1);
+			// Set cURL post options
+			curl_setopt($ch,CURLOPT_CUSTOMREQUEST, $Method);
+			curl_setopt ($ch, CURLOPT_POSTFIELDS, json_encode($post));
+		}
+		
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch,CURLOPT_ENCODING, '');
+		curl_setopt($ch,CURLOPT_MAXREDIRS, 10);
+		curl_setopt($ch,CURLOPT_TIMEOUT, 0);
+		curl_setopt($ch,CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch,CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+		curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
 
-			$response = curl_exec($ch);
-			#descomentar esto cuando todas las respuestas del servidos sean un json al final
-			// $result = ($response ? json_decode($response, true) : false);
-			
-			return $response;
+		$response = curl_exec($ch);
+		#descomentar esto cuando todas las respuestas del servidos sean un json al final
+		// $result = ($response ? json_decode($response, true) : false);
+		// array_unique($result);
+		// var_dump($response);
+		
+		return $response;
 	}
 
 	public function setAccesToken($access_token) {
 		$this->access_token = $access_token;
 	}
 
-	public function CreatePass($CampaignCode, $data = null, $Method = 'POST') {
-		return $this->doQueryDev("{$CampaignCode}/Pass/New", $data, $Method);
+	public function CreatePass($ActivityID, $data = null, $Method = 'POST') {
+		return $this->doQueryDev("{$ActivityID}/Pass/New", $data, $Method);
+	}
+
+	public function UpdatePass($ActivityID, $data) {
+		return $this->doQueryDev("{$ActivityID}/Pass/Update", $data);
 	}
 
 	public function LoadCertificate($Cerb64) {
@@ -98,5 +105,9 @@ class Wallet4Sales
 
 	public function UploadImage($ImageType,$data){
 		return $this->doQueryDev("{$ImageType}/UploadImg", $data);
+	}
+
+	public function GetKeysCard($CardCode){
+		return $this->doQueryDev("GetKeysCard/{$CardCode}");
 	}
 }
